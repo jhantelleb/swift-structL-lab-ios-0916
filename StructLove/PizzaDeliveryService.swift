4c4cb6a538d9e9dd9ddf11dc7a99f87ccd9900d4
Jhantelle Belleza <jhantellebelleza@Jhantelles-MacBook-Pro.local>
2016-10-10 23:37:40 -0400

    Completed Exercise

--- StructLove/PizzaDeliveryService.swift
+++ StructLove/PizzaDeliveryService.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+struct PizzaDeliveryService {
+    var location: Coordinate
+    var pizzasAvailable: Int = 10
+    
+    
+    init(location: Coordinate) {
+        self.location = location
+    }
+    
+    func isInRange(to destination: Coordinate) -> Bool
+    { return destination.distance(to: location) <= 5000.0 }
+    
+    mutating func deliverPizza(to destination: Coordinate) -> Bool
+    {   var canDeliver = true
+        if self.pizzasAvailable > 0 {
+        if isInRange(to: destination) {
+            self.pizzasAvailable -= 1
+            canDeliver = true
+            } else { canDeliver = false  }}
+        else { canDeliver = false }
+            
+        return canDeliver
+    }
+}
